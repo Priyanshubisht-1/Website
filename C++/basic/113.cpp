f9e939aef3b8b6b0a0ff9a2351e45e7b5dd8b1bc
Priyanshubisht-1 <166350065+Priyanshubisht-1@users.noreply.github.com>
2024-04-10 18:58:51 +0530

    Update 113.cpp

--- C++/basic/113.cpp
+++ C++/basic/113.cpp
@@ -1,1 +1,32 @@
-/*113. Create a class entering the command line arguments from the user and show all the arguments as output.*/
+#include <iostream>
+using namespace std;
+
+class cmar {
+    string* arguments;
+    int count;
+
+public:
+   cmar(int argc, char* argv[]) {
+        count = argc;
+        arguments = new string[count];
+        for (int i = 0; i < count; ++i) {
+            arguments[i] = argv[i];
+        }
+    }
+
+    void showArguments() {
+        cout << "Command-line arguments:" << endl;
+        for (int i = 1; i < count; ++i) {
+            cout << "Argument " << i << ": " << arguments[i] << endl;
+        }
+    }
+    ~cmar() {
+        delete[] arguments;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    cmar cmdArgs(argc, argv);
+    cmdArgs.showArguments();
+    return 0;
+}
